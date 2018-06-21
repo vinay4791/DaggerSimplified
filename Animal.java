9d298c4b95cab11405d5dc61ff016aae74058cbe
Vinay Mathew John <vinay.john@litmus7.com>
2018-06-21 13:37:33 +0530

    Initial commit

---
+++ Animal.java
@@ -0,0 +1,21 @@
+package com.example.vinayjohn.daggersimplified;
+
+/**
+ * Created by vinayjohn on 14/03/18.
+ */
+
+public class Animal {
+
+    private String type;
+
+    public Animal() {
+        this.type = "Animal";
+    }
+
+    public String getType() {
+        return type;
+    }
+}
+
+
+
